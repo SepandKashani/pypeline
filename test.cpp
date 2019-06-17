73aa206db4feeefeee12ee84d277c9cb481702fa
Sepand KASHANI <kashani.sepand@gmail.com>
2019-06-17 20:57:55 +0200

    [C++] Implement ffs::next_fast_len()

--- test.cpp
+++ test.cpp
@@ -4,11 +4,25 @@
 // Author : Sepand KASHANI [kashani.sepand@gmail.com]
 // ############################################################################
 
-#include <string>
 #include <iostream>
 
+#include "pypeline/ffs.hpp"
+
+
+void test_next_fast_len() {
+    namespace _ffs = pypeline::ffs;
+
+    size_t const target = 97;
+    size_t const out = _ffs::next_fast_len(target);
+
+    std::cout << target << std::endl;
+    std::cout << out << std::endl;
+}
+
 int main() {
-    std::cout << "Correct" << std::endl;
+    pybind11::scoped_interpreter guard{};
+
+    test_next_fast_len();
 
     return 0;
 }
