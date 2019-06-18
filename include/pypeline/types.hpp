a8eed22f89ff6d4e3fbea948379161fdb67b59f8
Sepand KASHANI <kashani.sepand@gmail.com>
2019-06-18 21:40:05 +0200

    [C++] Drop unused typedefs

--- include/pypeline/types.hpp
+++ include/pypeline/types.hpp
@@ -17,9 +17,6 @@
 #include "eigen3/Eigen/Sparse"
 
 namespace pypeline {
-    using cfloat_t = std::complex<float>;
-    using cdouble_t = std::complex<double>;
-
     template <typename T> using ArrayX_t   = Eigen::Array  <T,              1, Eigen::Dynamic, Eigen::RowMajor | Eigen::AutoAlign>;
     template <typename T> using ArrayXX_t  = Eigen::Array  <T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor | Eigen::AutoAlign>;
     template <typename T> using MatrixXX_t = Eigen::Matrix <T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor | Eigen::AutoAlign>;
