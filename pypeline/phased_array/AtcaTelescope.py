932f926466d1a9f12b5bd9877d61ce5cdbfb717c
Dewan Arun Singh <dewanarunsingh@outlook.com>
2020-01-30 16:09:20 +1100

    Defines AtcaTelescope in compatible format

--- pypeline/phased_array/AtcaTelescope.py
+++ pypeline/phased_array/AtcaTelescope.py
@@ -4,10 +4,15 @@
  # Author : Dewan Arun Singh (dewanarunsingh@outlook.com)       #
 # #############################################################
 
+
 import numpy as np
 import pandas as pd
+import pkg_resources as pkg
 import ImoT_tools.imot_tools.util.argcheck as chk
+
+from pathlib import Path
 from pypeline.phased_array.instrument import EarthBoundInstrumentGeometryBlock
+from pypeline.phased_array.instrument import _as_InstrumentGeometry
 
 class AtcaTelescope (EarthBoundInstrumentGeometryBlock):
     """
@@ -16,15 +21,27 @@ class AtcaTelescope (EarthBoundInstrumentGeometryBlock):
     ATCA consists of a single station, with 6 antennas out of which 5 are movable and can be positioned at 44 different locations
     along a 3KM East-West track and a 214m North-South track.
     """
-    @chk.check(dict(chk.)
-
-    def __init__(self, array_config = "6A", ):
+    #Constants
+    STATION_LOCATION = [-4750915.837, 2792906.182, -3200483.747]
+    def __init__(self, array_config):
         """
         Parameters
         _______________
-        array_config : string
-        One of the possible 20 configurations of the antennas at ATCA.
+        array_config : int
+        One of the possible 28 configurations of the antennas at ATCA. Each integer corresponds to an alphanumeric configuration
+        as provided for ATCA.
         ______________________________________________________________
         """
+        rel_path = Path("data", "phased_array", "instrument", "ATCA.csv")
+        abs_path = pkg.resource_filename("pypeline", str(rel_path))
+
+        if not Path(abs_path).is_file():
+            raise FileNotFoundError
+        else:
+            itrs_geom = pd.read_csv(abs_path).set_index('VARIANT','ANTENNA_ID')
+            XYZ = _as_InstrumentGeometry(itrs_geom)
+        
+        super().__init__(XYZ, array_config)
+
         
     
