21d08467d6b7459736458b71919374a40c684b18
Dewan Arun Singh <dewanarunsingh@outlook.com>
2020-01-31 11:48:31 +1100

    Added AtcaData.py

---
+++ pypeline/phased_array/AtcaData.py
@@ -0,0 +1,10 @@
+    # #################################################################
+   # AtcaData.py                                                    #
+  # ================                                              #
+ # Author : Dewan Arun Singh (dewanarunsingh@outlook.com)       #
+# #############################################################
+
+import numpy as np
+import pandas as pd
+import pypeline.phased_array.measurement_set as measurement_set
+
--- pypeline/phased_array/AtcaTelescope.py
+++ pypeline/phased_array/AtcaTelescope.py
@@ -38,9 +38,13 @@ class AtcaTelescope (EarthBoundInstrumentGeometryBlock):
         if not Path(abs_path).is_file():
             raise FileNotFoundError
         else:
+            # Get the ITRS geometry from the csv file and pass to super constructor. Not using "STATION_ONLY" as ATCA is a single
+            # station telescope.
             itrs_geom = pd.read_csv(abs_path).set_index('VARIANT','ANTENNA_ID')
             XYZ = _as_InstrumentGeometry(itrs_geom)
         
+        #Reconsidering using the super constructor as ATCA has only 1 station and might need to define it's own constructor. 
+        # Thoughts??
         super().__init__(XYZ, array_config)
 
         
