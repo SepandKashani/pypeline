ede067a0e35999bb6d4ecc8d60035e4a62567fcb
Dewan Arun Singh <dewanarunsingh@outlook.com>
2020-01-31 13:52:26 +1100

    Creates the instrument property function

--- pypeline/phased_array/AtcaData.py
+++ pypeline/phased_array/AtcaData.py
@@ -6,5 +6,71 @@
 
 import numpy as np
 import pandas as pd
-import pypeline.phased_array.measurement_set as measurement_set
+import imot_tools.util.argcheck as chk
+import casacore.tables as ct
+import pypeline.phased_array.instrument as instrument
+
+from pypeline.phased_array.measurement_set import MeasurementSet
+
+class AtcaData(MeasurementSet):
+    """ 
+    Australia Telescope Compact Array (ATCA) data handler class.
+    """
+    @chk.check(dict(fileName = chk.isinstance(str)))
+
+    def __init__(self, fileName):
+        """
+        Parameters
+        ----------
+        fileName : String
+        Path to the MS file for the data.
+
+        arrayConfig : int
+        A number corresponding to one of the 28 array configurations at ATCA.
+        """
+        super().__init__(fileName)
+        self._arrayConfig = arrayConfig
+        
+    @property
+    def instrument(self):
+        """
+        Returns
+        -------
+        :py:class:`~pypeline.phased_array.instrument.EarthBoundInstrumentGeometryBlock`
+            Computes Instrument position.
+        """
+        if self._instrument is None:
+            # Following the  MS file specification from, https://casa.nrao.edu/casadocs/casa-5.1.0/reference-material/measurement-set
+            # Some remarks on the required fields:
+            # - NAME: ID of the antenna
+            # - POSITION: Absolute positions of the antennas
+            # - FLAG_ROW: True/False value for each antenna. 
+            #             If any of the polarization flags is True for a given antenna, then the
+            #             antenna can be discarded from that configuration.
+
+            query = f'select NAME,POSITION,FLAG_ROW from {self._msf}::ANTENNA'
+            table = ct.taql(query)
+
+            #Getting the necessary data out from the ms table
+            antenna_name = table.getcol("NAME")
+            antenna_position = table.getcol("POSITION")
+            antenna_flag = table.getcol("FLAG_ROW")
+
+            #Creating a dataframe that holds the all the relevant data and filtering out flagged antennas.
+            antennae = np.reshape(antenna_position,(6,3))
+            
+            #Create a multi-level index for the new dataframe with STATION_ID always as 0
+            cfg_index = pd.MultiIndex.from_product([{0},antenna_name],names=['STATION_ID','ANTENNA_NAME']
+            )
+            df = pd.DataFrame(data=antennae, columns=['X','Y','Z'], index=cfg_index).loc[~antenna_flag]
+
+            #Creating instrument block for ATCA data specified.
+            XYZ = instrument.InstrumentGeometry(xyz=df.values, ant_idx=df.index)
+            self._instrument = instrument.EarthBoundInstrumentGeometryBlock(XYZ)
+        
+        return self._instrument
+    
+    
+
+
 
