8a654a853a762bba3d6b565a0e6ade6615a25e46
Dewan Arun Singh <dewanarunsingh@outlook.com>
2020-02-03 15:14:20 +1100

    Changes index to match parameter checks

--- pypeline/phased_array/AtcaTelescope.py
+++ pypeline/phased_array/AtcaTelescope.py
@@ -1,7 +1,7 @@
-     # #################################################################
-    # AtcaTelescope.py                                               #
-   # ================                                              #
-  # Author : Dewan Arun Singh (dewanarunsingh@outlook.com)      #
+    # #################################################################
+   # AtcaTelescope.py                                               #
+  # ================                                              #
+ # Author : Dewan Arun Singh (dewanarunsingh@outlook.com)       #
 # #############################################################
 
 
@@ -14,7 +14,7 @@ from pathlib import Path
 from pypeline.phased_array.instrument import EarthBoundInstrumentGeometryBlock
 from pypeline.phased_array.instrument import _as_InstrumentGeometry
 
-class AtcaTelescope (EarthBoundInstrumentGeometryBlock):
+class AtcaTelescope(EarthBoundInstrumentGeometryBlock):
     """
     Australian Telescope Compact Array (ATCA) <https://www.narrabri.atnf.csiro.au/> - Located in Narrabi, NSW, Australia
 
@@ -22,14 +22,21 @@ class AtcaTelescope (EarthBoundInstrumentGeometryBlock):
     along a 3KM East-West track and a 214m North-South track.
     
     This class is to be used only for the simulated data. 
+    ---------------------------------------------------------------
+    Parameters
+    ============================
+    variant : string
+    One of the 20 possible configurations for ATCA telescope, can be found at 
+    https://www.narrabri.atnf.csiro.au/observing/users_guide/html/atug.html#ATCA-Array-Configurations
+    
     """
     
-    def __init__(self, array_config):
+    def __init__(self, variant):
         """
         Parameters
         -------------
-        array_config : int
-        One of the possible 28 configurations of the antennas at ATCA. Each integer corresponds to an alphanumeric configuration
+        variant : string
+        One of the possible 20 configurations of the antennas at ATCA. Each integer corresponds to an alphanumeric configuration
         as provided for ATCA.
         -------------------
         """
@@ -41,12 +48,13 @@ class AtcaTelescope (EarthBoundInstrumentGeometryBlock):
         else:
             # Get the ITRS geometry from the csv file and pass to super constructor. Not using "STATION_ONLY" as ATCA is a single
             # station telescope.
-            itrs_geom = pd.read_csv(abs_path).set_index('VARIANT','ANTENNA_ID')
-            XYZ = _as_InstrumentGeometry(itrs_geom)
-        
-        #Reconsidering using the super constructor as ATCA has only 1 station and might need to define it's own constructor. 
-        # Thoughts??
-        super().__init__(XYZ, array_config)
+            itrs_geom = pd.read_csv(abs_path).set_index(['VARIANT','ANTENNA_ID'])
+            # XYZ = _as_InstrumentGeometry(itrs_geom)
+            geom_values = itrs_geom.iloc[itrs_geom.index.get_level_values('VARIANT')==variant].values
+            ant_idx = pd.MultiIndex.from_product([{0},np.arange(1,7,1)],names=['STATION_ID','ANTENNA_ID'])
+            XYZ = _as_InstrumentGeometry(pd.DataFrame(data=geom_values,index=ant_idx))
+                
+        super().__init__(XYZ, N_station=1)
 
         
     
