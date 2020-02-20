3673c8fb8f917b1ff39772fa5a76c5b4e3f47eac
Dewan Arun Singh <dewanarunsingh@outlook.com>
2020-02-20 13:17:20 +1100

    Makes changes to minimize imports for efficiency
    Utilizes previous scripts and transforms them to support function call style execution.

--- examples/simulation/atca.py
+++ examples/simulation/atca.py
@@ -18,6 +18,7 @@ import imot_tools.math.sphere.transform as transform
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as constants
+import imot_tools.util.argcheck as chk
 
 import pypeline.phased_array.beamforming as beamforming
 import pypeline.phased_array.bluebild.data_processor as bb_dp
@@ -30,94 +31,91 @@ import pypeline.phased_array.data_gen.statistics as statistics
 
 import pypeline.phased_array.Atca.AtcaTelescope as instrument
 
+@chk.check(dict(start=chk.is_instance(float),fc=chk.is_array_like,FoV=chk.is_instance(float),freq=chk.is_instance(float),
+            N_station=chk.is_integer,telescope=chk.is_instance(str),variant=chk.is_instance(str)))
+def atca(start, fc, FoV, freq, N_station, telescope,variant=None):
+    # Observation
+    obs_start = atime.Time(start, scale="utc", format="mjd")
+    field_center=coord.SkyCoord(fc[0] * u.rad, fc[1] * u.rad)
+    FoV = np.deg2rad(FoV)
+    wl = constants.speed_of_light/freq
+    # Instrument
+    dev=instrument.AtcaTelescope(variant=variant)
+    mb_cfg = [(_, _, field_center) for _ in range(N_station)]
+    mb = beamforming.MatchedBeamformerBlock(mb_cfg)
+    gram = bb_gr.GramBlock()
 
-
-# Observation
-obs_start = atime.Time(55679.80133020458, scale="utc", format="mjd")
-field_center=coord.SkyCoord(-0.807622 * u.rad, -0.65556 * u.rad)
-FoV, frequency = np.deg2rad(0.39), 2867e+06
-wl = constants.speed_of_light/frequency
-
-# Instrument
-N_station = 6
-#ms_file = "/home/das/ATNF_data/ms_data/2051-377-2868.ms/"
-#ms = data.AtcaData(ms_file)
-dev=instrument.AtcaTelescope("6A")
-mb_cfg = [(_, _, field_center) for _ in range(N_station)]
-mb = beamforming.MatchedBeamformerBlock(mb_cfg)
-gram = bb_gr.GramBlock()
-
-# Data generation
-T_integration = 10
-sky_model = source.SkyEmission([(coord.SkyCoord('20h54m54.171s -37d33m51.11s', frame='icrs'),0.15)])
-vis = statistics.VisibilityGeneratorBlock(sky_model, T_integration, fs=196000, SNR=np.inf)
-time = obs_start + (T_integration * u.s) * np.arange(815)
-#time = ms.time['TIME']
-#import pdb; pdb.set_trace()
-obs_end = time[-1]
-
-# Imaging
-N_level = 4
-N_bits = 32
-
-px_grid = grid.uniform(direction=field_center.cartesian.xyz.value,FoV=FoV,size=[600,600])
-
-
-### Intensity Field ===========================================================
-# Parameter Estimation
-I_est = bb_pe.IntensityFieldParameterEstimator(N_level, sigma=1)
-for t in ProgressBar(time[::200]):
-    XYZ = dev(t)
-    W = mb(XYZ, wl)
-    S = vis(XYZ, W, wl)
-    G = gram(XYZ, W, wl)
+    # Data generation
+    T_integration = 10
+    sky_model = source.SkyEmission([(coord.SkyCoord('20h54m54.171s -37d33m51.11s', frame='icrs'),0.15)])
+    vis = statistics.VisibilityGeneratorBlock(sky_model, T_integration, fs=196000, SNR=np.inf)
+    time = obs_start + (T_integration * u.s) * np.arange(815)
+    #time = ms.time['TIME']
     #import pdb; pdb.set_trace()
-    I_est.collect(S, G)
-N_eig, c_centroid = I_est.infer_parameters()
+    obs_end = time[-1]
 
-# Imaging
-I_dp = bb_dp.IntensityFieldDataProcessorBlock(N_eig, c_centroid)
-I_mfs =bb_sd.Spatial_IMFS_Block(wl,px_grid,N_level,N_bits)
-for t in ProgressBar(time[::1]):
-    XYZ = dev(t)
-    W = mb(XYZ, wl)
-    S = vis(XYZ, W, wl)
-    G = gram(XYZ, W, wl)
+    # Imaging
+    N_level = 4
+    N_bits = 32
 
-    D, V, c_idx = I_dp(S, G)
-    _ = I_mfs(D, V, XYZ.data, W.data, c_idx)
-I_std, I_lsq = I_mfs.as_image()
+    px_grid = grid.uniform(direction=field_center.cartesian.xyz.value,FoV=FoV,size=[600,600])
 
-### Sensitivity Field =========================================================
-# Parameter Estimation
-S_est = bb_pe.SensitivityFieldParameterEstimator(sigma=1)
-for t in ProgressBar(time[::200]):
-    XYZ = dev(t)
-    W = mb(XYZ, wl)
-    G = gram(XYZ, W, wl)
 
-    S_est.collect(G)
-N_eig = S_est.infer_parameters()
+    ### Intensity Field ===========================================================
+    # Parameter Estimation
+    I_est = bb_pe.IntensityFieldParameterEstimator(N_level, sigma=1)
+    for t in ProgressBar(time[::200]):
+        XYZ = dev(t)
+        W = mb(XYZ, wl)
+        S = vis(XYZ, W, wl)
+        G = gram(XYZ, W, wl)
+        #import pdb; pdb.set_trace()
+        I_est.collect(S, G)
+    N_eig, c_centroid = I_est.infer_parameters()
 
-# Imaging
-S_dp = bb_dp.SensitivityFieldDataProcessorBlock(N_eig)
-S_mfs = bb_sd.Spatial_IMFS_Block(wl,px_grid,1,N_bits)
-for t in ProgressBar(time[::50]):
-    XYZ = dev(t)
-    W = mb(XYZ, wl)
-    G = gram(XYZ, W, wl)
+    # Imaging
+    I_dp = bb_dp.IntensityFieldDataProcessorBlock(N_eig, c_centroid)
+    I_mfs =bb_sd.Spatial_IMFS_Block(wl,px_grid,N_level,N_bits)
+    for t in ProgressBar(time[::1]):
+        XYZ = dev(t)
+        W = mb(XYZ, wl)
+        S = vis(XYZ, W, wl)
+        G = gram(XYZ, W, wl)
 
-    D, V = S_dp(G)
-    _ = S_mfs(D, V, XYZ.data, W.data, cluster_idx=np.zeros(N_eig, dtype=int))
-_, S = S_mfs.as_image()
+        D, V, c_idx = I_dp(S, G)
+        _ = I_mfs(D, V, XYZ.data, W.data, c_idx)
+    I_std, I_lsq = I_mfs.as_image()
 
-# Plot Results ================================================================
-fig, ax = plt.subplots(ncols=2)
-I_std_eq = s2image.Image(I_std.data / S.data, I_std.grid)
-I_std_eq.draw(catalog=sky_model.xyz.T, ax=ax[0],show_gridlines=False)
-ax[0].set_title("Bluebild Standardized Image")
+    ### Sensitivity Field =========================================================
+    # Parameter Estimation
+    S_est = bb_pe.SensitivityFieldParameterEstimator(sigma=1)
+    for t in ProgressBar(time[::200]):
+        XYZ = dev(t)
+        W = mb(XYZ, wl)
+        G = gram(XYZ, W, wl)
 
-I_lsq_eq = s2image.Image(I_lsq.data / S.data, I_lsq.grid)
-I_lsq_eq.draw(catalog=sky_model.xyz.T, ax=ax[1], show_gridlines=False)
-ax[1].set_title("Bluebild Least-Squares Image")
-fig.show()
+        S_est.collect(G)
+    N_eig = S_est.infer_parameters()
+
+    # Imaging
+    S_dp = bb_dp.SensitivityFieldDataProcessorBlock(N_eig)
+    S_mfs = bb_sd.Spatial_IMFS_Block(wl,px_grid,1,N_bits)
+    for t in ProgressBar(time[::50]):
+        XYZ = dev(t)
+        W = mb(XYZ, wl)
+        G = gram(XYZ, W, wl)
+
+        D, V = S_dp(G)
+        _ = S_mfs(D, V, XYZ.data, W.data, cluster_idx=np.zeros(N_eig, dtype=int))
+    _, S = S_mfs.as_image()
+
+    # Plot Results ================================================================
+    fig, ax = plt.subplots(ncols=2)
+    I_std_eq = s2image.Image(I_std.data / S.data, I_std.grid)
+    I_std_eq.draw(catalog=sky_model.xyz.T, ax=ax[0],show_gridlines=False)
+    ax[0].set_title("Bluebild Standardized Image")
+
+    I_lsq_eq = s2image.Image(I_lsq.data / S.data, I_lsq.grid)
+    I_lsq_eq.draw(catalog=sky_model.xyz.T, ax=ax[1], show_gridlines=False)
+    ax[1].set_title("Bluebild Least-Squares Image")
+    fig.show()
--- examples/simulation/simulation.py
+++ examples/simulation/simulation.py
@@ -4,33 +4,6 @@
 # Author : Dewan Arun Singh (19884240@student.westernsydney.edu.au)
 #####################################################################
 
-from tqdm import tqdm as ProgressBar
-import astropy.coordinates as coord
-import astropy.time as atime
-import astropy.units as u
-import imot_tools.io.s2image as s2image
-import imot_tools.math.sphere.grid as grid
-import imot_tools.math.sphere.transform as transform
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy.constants as constants
-import imot_tools.util.argcheck as chk
-
-import pypeline.phased_array.beamforming as beamforming
-import pypeline.phased_array.bluebild.data_processor as bb_dp
-import pypeline.phased_array.bluebild.gram as bb_gr
-import pypeline.phased_array.bluebild.imager.spatial_domain as bb_sd
-import pypeline.phased_array.bluebild.parameter_estimator as bb_pe
-import pypeline.phased_array.data_gen.source as source
-import pypeline.phased_array.data_gen.statistics as statistics
-
-
-import pypeline.phased_array.Atca.AtcaTelescope as instrument
-
-TELESCOPES = ['atca','lofar','mwa']
-
-@chk.check(dict(start=chk.is_instance(float),fc=chk.is_array_like,FoV=chk.is_instance(float),freq=chk.is_instance(float),
-            N_station=chk.is_integer,telescope=chk.is_instance(str),variant=chk.is_instance(str)))
 def simulate(start, fc, FoV, freq, N_station, telescope,variant=None):
     """
     interative function for simulating telescopes in pypeline.
@@ -49,99 +22,15 @@ def simulate(start, fc, FoV, freq, N_station, telescope,variant=None):
     telescope : string
                 Name of the telescope. Currently supported telescopes are: atca, lofar, & mwa.
     variant : string
-                Only applies if you are imaging ATCA telescope. Is string consisting of one of the possible 20
-                configurations in the ATCA configuration array.
+            Only applies if you are imaging ATCA telescope. Is string consisting of one of the possible 20 configurations in the ATCA configuration array.
+    ----------------------------------------------
+
     """
-   
-    # Observation
-    obs_start = atime.Time(start, scale="utc", format="mjd")
-    field_center=coord.SkyCoord(fc[0] * u.rad, fc[1] * u.rad)
-    FoV = np.deg2rad(FoV)
-    wl = constants.speed_of_light/freq
-    # Instrument
-    #ms_file = "/home/das/ATNF_data/ms_data/2051-377-2868.ms/"
-    #ms = data.AtcaData(ms_file)
+    TELESCOPES = ['atca','lofar','mwa']
     if not telescope.lower() in TELESCOPES:
         raise ValueError (f"The value in telescope argument should be one of {TELESCOPES}")
     else:
         if telescope.lower() == 'atca':
-            dev=instrument.AtcaTelescope(variant=variant)
-        
-    mb_cfg = [(_, _, field_center) for _ in range(N_station)]
-    mb = beamforming.MatchedBeamformerBlock(mb_cfg)
-    gram = bb_gr.GramBlock()
+            import examples.simulation.atca as atca
+            atca.atca(start, fc, FoV, freq, N_station, telescope,variant)            
 
-    # Data generation
-    T_integration = 10
-    sky_model = source.SkyEmission([(coord.SkyCoord('20h54m54.171s -37d33m51.11s', frame='icrs'),0.15)])
-    vis = statistics.VisibilityGeneratorBlock(sky_model, T_integration, fs=196000, SNR=np.inf)
-    time = obs_start + (T_integration * u.s) * np.arange(815)
-    #time = ms.time['TIME']
-    #import pdb; pdb.set_trace()
-    obs_end = time[-1]
-
-    # Imaging
-    N_level = 4
-    N_bits = 32
-
-    px_grid = grid.uniform(direction=field_center.cartesian.xyz.value,FoV=FoV,size=[600,600])
-
-
-    ### Intensity Field ===========================================================
-    # Parameter Estimation
-    I_est = bb_pe.IntensityFieldParameterEstimator(N_level, sigma=1)
-    for t in ProgressBar(time[::200]):
-        XYZ = dev(t)
-        W = mb(XYZ, wl)
-        S = vis(XYZ, W, wl)
-        G = gram(XYZ, W, wl)
-        #import pdb; pdb.set_trace()
-        I_est.collect(S, G)
-    N_eig, c_centroid = I_est.infer_parameters()
-
-    # Imaging
-    I_dp = bb_dp.IntensityFieldDataProcessorBlock(N_eig, c_centroid)
-    I_mfs =bb_sd.Spatial_IMFS_Block(wl,px_grid,N_level,N_bits)
-    for t in ProgressBar(time[::1]):
-        XYZ = dev(t)
-        W = mb(XYZ, wl)
-        S = vis(XYZ, W, wl)
-        G = gram(XYZ, W, wl)
-
-        D, V, c_idx = I_dp(S, G)
-        _ = I_mfs(D, V, XYZ.data, W.data, c_idx)
-    I_std, I_lsq = I_mfs.as_image()
-
-    ### Sensitivity Field =========================================================
-    # Parameter Estimation
-    S_est = bb_pe.SensitivityFieldParameterEstimator(sigma=1)
-    for t in ProgressBar(time[::200]):
-        XYZ = dev(t)
-        W = mb(XYZ, wl)
-        G = gram(XYZ, W, wl)
-
-        S_est.collect(G)
-    N_eig = S_est.infer_parameters()
-
-    # Imaging
-    S_dp = bb_dp.SensitivityFieldDataProcessorBlock(N_eig)
-    S_mfs = bb_sd.Spatial_IMFS_Block(wl,px_grid,1,N_bits)
-    for t in ProgressBar(time[::50]):
-        XYZ = dev(t)
-        W = mb(XYZ, wl)
-        G = gram(XYZ, W, wl)
-
-        D, V = S_dp(G)
-        _ = S_mfs(D, V, XYZ.data, W.data, cluster_idx=np.zeros(N_eig, dtype=int))
-    _, S = S_mfs.as_image()
-
-    # Plot Results ================================================================
-    fig, ax = plt.subplots(ncols=2)
-    I_std_eq = s2image.Image(I_std.data / S.data, I_std.grid)
-    I_std_eq.draw(catalog=sky_model.xyz.T, ax=ax[0],show_gridlines=False)
-    ax[0].set_title("Bluebild Standardized Image")
-
-    I_lsq_eq = s2image.Image(I_lsq.data / S.data, I_lsq.grid)
-    I_lsq_eq.draw(catalog=sky_model.xyz.T, ax=ax[1], show_gridlines=False)
-    ax[1].set_title("Bluebild Least-Squares Image")
-    fig.show()
