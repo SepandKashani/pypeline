324169dafc2a240ecbfa6ef3192f3b781954f523
dasingh92 <dewanarunsingh@outlook.com>
2020-08-16 18:36:50 +1000

    Creates atca_ps script and adds time check to hardcode scripts

--- examples/simulation/atca_hardcode_ps.py
+++ examples/simulation/atca_hardcode_ps.py
@@ -1,11 +1,11 @@
-#####################################################################
-# atca_hardcode.py
-# ================
-# Author : Dewan Arun Singh (19884240@student.westernsydney.edu.au)
-#####################################################################
+# #############################################################################
+# atca_harcode_ps.py
+# ==================
+# Author : Dewan Arun Singh [19884240@student.westernsydney.edu.au]
+# #############################################################################
 
 """
-Simulated ATCA imaging with Bluebild (Standard Synthesis).
+Simulated ATCA imaging with Bluebild (PeriodicSynthesis).
 """
 
 from tqdm import tqdm as ProgressBar
@@ -14,24 +14,19 @@ import astropy.time as atime
 import astropy.units as u
 import imot_tools.io.s2image as s2image
 import imot_tools.math.sphere.grid as grid
-import imot_tools.math.sphere.transform as transform
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as constants
-import pandas as pd
 import datetime
-import imot_tools.util.argcheck as chk
+
 import pypeline.phased_array.beamforming as beamforming
 import pypeline.phased_array.bluebild.data_processor as bb_dp
 import pypeline.phased_array.bluebild.gram as bb_gr
-import pypeline.phased_array.bluebild.imager.spatial_domain as bb_sd
+import pypeline.phased_array.bluebild.imager.fourier_domain as bb_fd
 import pypeline.phased_array.bluebild.parameter_estimator as bb_pe
 import pypeline.phased_array.data_gen.source as source
 import pypeline.phased_array.data_gen.statistics as statistics
-
-
 import pypeline.phased_array.Atca.AtcaTelescope as instrument
-
 
 
 # Observation
@@ -40,12 +35,12 @@ obs_start = atime.Time(55679.80133020458, scale="utc", format="mjd")
 field_center=coord.SkyCoord(-0.807622 * u.rad, -0.65556 * u.rad)
 FoV,freq = np.deg2rad(0.39), 2867e+06
 wl = constants.speed_of_light/freq
+
 # Instrument
 N_station=6
 dev=instrument.AtcaTelescope(variant='6A')
 mb_cfg = [(_, _, field_center) for _ in range(N_station)]
 mb = beamforming.MatchedBeamformerBlock(mb_cfg)
-#import pdb; pdb.set_trace()
 gram = bb_gr.GramBlock()
 
 # Data generation
@@ -61,9 +56,13 @@ obs_end = time[-1]
 # Imaging
 N_level = 4
 N_bits = 32
-
-px_grid = grid.uniform(direction=field_center.cartesian.xyz.value,FoV=FoV,size=[600,600])
-
+R = dev.icrs2bfsf_rot(obs_start, obs_end)
+_, _, pix_colat, pix_lon = grid.equal_angle(
+    N=dev.nyquist_rate(wl),
+    direction=R @ field_center.cartesian.xyz.value,  # BFSF-equivalent f_dir.
+    FoV=FoV,
+)
+N_FS, T_kernel = dev.bfsf_kernel_bandwidth(wl, obs_start, obs_end), FoV*2
 
 ### Intensity Field ===========================================================
 # Parameter Estimation
@@ -73,29 +72,21 @@ for t in ProgressBar(time[::200]):
     W = mb(XYZ, wl)
     S = vis(XYZ, W, wl)
     G = gram(XYZ, W, wl)
-    #import pdb; pdb.set_trace()
+
     I_est.collect(S, G)
 N_eig, c_centroid = I_est.infer_parameters()
 
 # Imaging
 I_dp = bb_dp.IntensityFieldDataProcessorBlock(N_eig, c_centroid)
-I_mfs =bb_sd.Spatial_IMFS_Block(wl,px_grid,N_level,N_bits)
-sim_data = np.empty([0,],dtype=np.complex64)
+I_mfs = bb_fd.Fourier_IMFS_Block(wl, pix_colat, pix_lon, N_FS, T_kernel, R, N_level, N_bits)
 for t in ProgressBar(time[::1]):
     XYZ = dev(t)
     W = mb(XYZ, wl)
     S = vis(XYZ, W, wl)
     G = gram(XYZ, W, wl)
-    if t==55679.8955431675:
-        np.save('/home/das/Radio_Astronomy/simulated_visibility', S.data)
-    sim = np.triu(S.data,k=1)
-    i,j = [0,0,1,0,1,2,0,1,2,3,0,1,2,3,4],[1,2,2,3,3,3,4,4,4,4,5,5,5,5,5]
-    for x,y in zip(i,j):
-        sim_data = np.append(sim_data,sim[x,y])
 
     D, V, c_idx = I_dp(S, G)
     _ = I_mfs(D, V, XYZ.data, W.data, c_idx)
-#np.save('/home/das/Radio_Astronomy/sim_data',sim_data)    
 I_std, I_lsq = I_mfs.as_image()
 
 ### Sensitivity Field =========================================================
@@ -111,7 +102,7 @@ N_eig = S_est.infer_parameters()
 
 # Imaging
 S_dp = bb_dp.SensitivityFieldDataProcessorBlock(N_eig)
-S_mfs = bb_sd.Spatial_IMFS_Block(wl,px_grid,1,N_bits)
+S_mfs = bb_fd.Fourier_IMFS_Block(wl, pix_colat, pix_lon, N_FS, T_kernel, R, 1, N_bits)
 for t in ProgressBar(time[::50]):
     XYZ = dev(t)
     W = mb(XYZ, wl)
@@ -122,13 +113,14 @@ for t in ProgressBar(time[::50]):
 _, S = S_mfs.as_image()
 
 print("The execution time for the script excluding the plotting of the image is: ",(datetime.datetime.now() - begin_time))
+
 # Plot Results ================================================================
 fig, ax = plt.subplots(ncols=2)
 I_std_eq = s2image.Image(I_std.data / S.data, I_std.grid)
-I_std_eq.draw(catalog=sky_model.xyz.T, ax=ax[0],show_gridlines=False)
+I_std_eq.draw(catalog=sky_model.xyz.T, ax=ax[0])
 ax[0].set_title("Bluebild Standardized Image")
 
 I_lsq_eq = s2image.Image(I_lsq.data / S.data, I_lsq.grid)
-I_lsq_eq.draw(catalog=sky_model.xyz.T, ax=ax[1], show_gridlines=False)
+I_lsq_eq.draw(catalog=sky_model.xyz.T, ax=ax[1])
 ax[1].set_title("Bluebild Least-Squares Image")
 fig.show()
